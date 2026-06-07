5bd3dfb80baafa04ec2f7a9b587e3886d2d2f5ab
Fahri Rivaldy <fahrirvaldy69@gmail.com>
2026-06-08 01:27:12 +0700

    set performace

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,16 @@ export default defineConfig({
   build: {
     target: 'es2022',
     cssTarget: 'es2022',
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        // Logika pemecahan aset vendor agar bundle tidak menumpuk dalam satu file
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
